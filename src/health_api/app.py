a64aed82a2eae25fa1e46f83b91c9d237e26c7b9
pochuan <pochuant@usc.edu>
2020-02-08 22:11:11 -0800

    modify the template

---
+++ src/health_api/app.py
@@ -0,0 +1,4 @@
+from health_api.serving import Server
+
+if __name__ == "__main__":
+    Server.start()
--- src/health_api/environment.py
+++ src/health_api/environment.py
@@ -10,6 +10,8 @@ class HostEnvironment(object):
     JOB_NAME_ENV = "JOB_NAME"
     USE_NGINX_ENV = "USE_NGINX"
     HEALTH_SSL_AUTH_ENABLED = "HEALTH_SSL_AUTH_ENABLED"
+    GUNICORN_SERVER_WORKER_TIMEOUT = "GUNICORN_SERVER_WORKER_TIMEOUT"
+    GUNICORN_SERVER_WORKER_NUM = "GUNICORN_SERVER_WORKER_NUM"
 
     def __init__(self):
         
@@ -22,7 +24,9 @@ class HostEnvironment(object):
         # Enabled nginx if set to True
         self.use_nginx = os.environ.get("USE_NGINX", False)
 
+        # Define web server worker timeout
+        self.server_worker_timeout = os.environ.get("GUNICORN_SERVER_WORKER_TIMEOUT", 60)
 
 
-
-
+        # Define web server worker number(s)
+        self.server_worker_num = os.environ.get("GUNICORN_SERVER_WORKER_NUM", 1)
